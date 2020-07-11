32c95865090f415f1fd0fd21216c9e1f0f605a04
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-11 23:54:39 +0600

    CheckVowelAndConsonant

---
+++ CheckVowelAndConsonant.java
@@ -0,0 +1,27 @@
+
+package basic;
+
+import java.util.Scanner;
+
+public class CheckVowelAndConsonant {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char letter;
+        System.out.print("Enter the Letter: ");
+        letter = input.next().charAt(0);
+        if(letter=='a'){
+            System.out.println("Vowel");
+        }else if(letter=='e'){
+            System.out.println("Vowel");
+        }
+        else if(letter=='i'){
+            System.out.println("Vowel");
+        }else if(letter=='o'){
+            System.out.println("Vowel");
+        }else if(letter=='u'){
+            System.out.println("Vowel");
+        }else{
+        System.out.println("Consonant");
+        }
+    }
+}
