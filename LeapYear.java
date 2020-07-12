79aa627c05b20b9bd9c4f8541830bb61aa3ce5b9
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-12 15:32:06 +0600

    Leap year

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+package basic;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Year: ");
+        int year=input.nextInt();
+        //Leap year kina bujhar main upai 4 diya vag jabe and 100 vag jabe na
+        // ba 400 vag jabe valo 
+        if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){  
+        System.out.println("LEAP YEAR");  
+        }  
+        else{  
+            System.out.println("COMMON YEAR");  
+        }
+        
+    }
+}
