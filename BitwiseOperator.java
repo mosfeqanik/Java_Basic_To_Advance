12ece5c23774ea6558d48dd164b6482323cb3e11
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-12 21:28:31 +0600

    Bitwise Operator

---
+++ BitwiseOperator.java
@@ -0,0 +1,27 @@
+
+package basic;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 12;
+        
+        int c;
+        
+        c = a&b;    
+        //and e Multiplication hoi bit wise
+        System.out.println("a & b :" +c);
+         
+        c = a|b;    
+        //OR e Plus hoi bit wise
+        System.out.println("a | b :" +c);
+        
+        c = a^b;    
+        //EXOR e akta one thakle  result e one  hoi bit wise
+        System.out.println("a ^ b :" +c);
+        
+    
+        
+    }
+ 
+}
