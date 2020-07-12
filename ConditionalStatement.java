acf7bae434f9ccc1408066412fb5dd43fc4e7aff
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-12 15:24:31 +0600

    ConditionalStatementAndTernaryOperator

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic;
-
-/**
- *
- * @author MN
- */
+import java.util.Scanner;
 public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1,num2,large;
+        System.out.print("Enter Two numbers: ");
+        num1=input.nextInt();
+        num2=input.nextInt();
+        //ternary operator
+        
+        //(Condition)? 1st condition result :else result ;
+        large =(num1>num2)?num1:num2;
+        System.out.println("The largest number is : "+ large);
+    }
     
 }
