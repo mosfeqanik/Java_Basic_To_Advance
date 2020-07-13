efe80b9e84c46f0d58d2af4f43853fc040a32aea
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 18:17:56 +0600

    while loop

--- WhileLoop.java
+++ WhileLoop.java
@@ -4,8 +4,18 @@ public class WhileLoop {
     public static void main(String[] args) {
         int i = 1;
         while(i<=5){
-            System.out.println(i+"Bangladesh");
+            System.out.println(i+"."+"Bangladesh");
             i++;
+        }
+        i =2;
+        while(i<=100){
+            System.out.println(i+"Even number");
+            i=i+2;
+        }
+        i =100;
+        while(i>=2){
+            System.out.println(i+"Even number");
+            i=i-2;
         }
     }
 }
