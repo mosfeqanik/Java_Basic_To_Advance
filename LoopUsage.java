291a690e4b4115a7755d77160d1ce934de4fed56
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 18:08:07 +0600

    For loop

---
+++ LoopUsage.java
@@ -0,0 +1,18 @@
+
+package basic;
+
+public class LoopUsage {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            //to print 5 bangladesh
+            System.out.println(i+".Bangladesh");
+        }
+        for (int i = 2; i <=100; i=i+2) {
+            System.out.println(i+"Even number");
+        }
+        for (int i = 100; i >=2; i=i-2) {
+            System.out.println(i+"Even number");
+        }
+    }
+    
+}
