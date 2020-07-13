c00b4da44c1b6becf82d684710d4ac68b6aa753d
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 18:14:28 +0600

    while loop

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=5){
+            System.out.println(i+"Bangladesh");
+            i++;
+        }
+    }
+}
