a8fc1c308bb1d5953af72931e04774ace33547a1
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 21:54:28 +0600

    break and COntinue example

---
+++ BreakContinueDemo.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class BreakContinueDemo {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i==10) {
+                break;
+                //Break er time for loop ar kaj korbe na 
+            }
+            System.out.println(i);
+        }
+        System.out.println("-----------------------------------------------");
+        
+        for (int i = 1; i <= 100; i++) {
+            if (i==10) {
+                continue;
+                //Continue er time for loop e oi time badh porbe  
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
