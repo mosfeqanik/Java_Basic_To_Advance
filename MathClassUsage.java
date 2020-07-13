bf7a991fa8432f0e07f8cd225517826360138f62
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 17:13:29 +0600

    Math class Usage

--- MathClassUsage.java
+++ MathClassUsage.java
@@ -8,19 +8,29 @@ public class MathClassUsage {
         x = input.nextInt();
         y = input.nextInt();
         
-        System.out.println(Math.max(x, y));
-        System.out.println(Math.min(x,y));
+        System.out.println("max between "+x+" and "+y+ " "+Math.max(x, y));
+        System.out.println("min between "+x+" and "+y+ " "+Math.min(x,y));
         
         System.out.print("Enter A negative Number to get a absolute number: ");
         a = input.nextInt();
         int absolute = Math.abs(a);
         System.out.println(absolute);
         
-        System.out.print("Enter a desire power number ");
+        System.out.print("Enter a desire power number :");
         double powernumber= input.nextInt();
         double basenumber = input.nextInt();
         
         double power= Math.pow(powernumber, basenumber);
-        System.out.println( basenumber +" power "+ powernumber +" : "+ power);
+        System.out.println( basenumber +" to the power of "+ powernumber +" : "+ power);
+        
+        System.out.print("Enter a Decimal number : ");
+        double number=input.nextDouble();
+        double floorResult=Math.floor(number);
+        System.out.println("floor result of decimal number is: "+floorResult);
+                
+        double pi =Math.PI;
+        System.out.println("PI resuSlt : "+pi);
+
+    
     }
 }
