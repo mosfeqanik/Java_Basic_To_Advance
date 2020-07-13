8bf4db356b5b30b034996666b0d6dd254dfb898e
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-13 18:38:27 +0600

    Do while loop Usage

---
+++ DowhileLoopUsage.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class DowhileLoopUsage {
+    public static void main(String[] args) {
+        
+        int i=1;
+        do{
+            System.out.println(i+".Bangladesh");
+            i++;
+        }while(i<=5);
+        
+        i =2;
+        do{
+            System.out.println(i+"Even number");
+            i=i+2;
+        }while(i <=100);
+        
+        i =100;
+        do{
+            System.out.println(i+"Even number");
+            i=i-2;
+        }while(i >=2);
+        
+        
+        
+        
+    }
+}
