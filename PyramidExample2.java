75ef84fa10a7b2d90a3fa12283aaeed533789a48
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-16 22:18:22 +0600

    Pyramid Example

---
+++ PyramidExample2.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class PyramidExample2 {
+   public static void main(String[] args) {  
+        int term=6;  
+        for(int i=1;i<=term;i++){  
+            for(int j=term;j>=i;j--){  
+                    System.out.print("* ");  
+                }  
+        System.out.println();//new line  
+        }  
+        for(int i=1;i<=term;i++){  
+            for(int j=term;j>=i;j--){  
+                    System.out.print(i);  
+                }  
+        System.out.println();//new line  
+        }  
+    }   
+}
