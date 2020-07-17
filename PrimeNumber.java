b75cff23ade1f57eda91ef9a05d72f6d6a9ed3e1
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-17 22:15:16 +0600

    Prime Number Check

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,6 +6,7 @@ public class PrimeNumber {
         System.out.print("Enter Any Positive Integer ");
         Scanner input = new Scanner(System.in);
         int num=input.nextInt();
+        //to check this number is prime number is not or is
         int count=0;
         for (int i = 2; i < num; i++) {
             if (num%i==0) {
