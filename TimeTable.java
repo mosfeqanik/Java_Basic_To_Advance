e0ee69aa91f9f159cce1ca1578185fcf22cdc9c4
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-17 12:04:24 +0600

    Time Table Example Done

---
+++ TimeTable.java
@@ -0,0 +1,18 @@
+
+package basic;
+import java.util.Scanner;
+
+public class TimeTable {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Desire number TIME TABLE ");
+        int n=input.nextInt();
+        int multipication;
+        for (int i = 1; i <= 10; i++) {
+            multipication=i*n;
+            System.out.println(n +"X"+ i+"="+multipication);
+        }
+        
+    }
+}
