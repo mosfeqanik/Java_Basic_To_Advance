f8ed3b39e22d48a92fa1a0132a7de8adeae580a2
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-21 22:22:17 +0600

    Pattern10 Small Letter Pyramid

--- Pattern10.java
+++ Pattern10.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic;
 import java.util.Scanner;
-/**
- *
- * @author MN
- */
 public class Pattern10 {
 
     public static void main(String[] args) {
@@ -25,6 +16,5 @@ public class Pattern10 {
             }
             System.out.println(" ");
         }
-        
     }
 }
