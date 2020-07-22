7d2e80b46e3b6084fe103c9323e06d2ac13d4463
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-22 18:30:15 +0600

    Pattern Arrow

---
+++ Pattern24ArrowExample.java
@@ -0,0 +1,23 @@
+package basic;
+import java.util.Scanner;
+
+public class Pattern24ArrowExample {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter Arrow size: ");
+            int size=input.nextInt();
+            
+            for (int row = 1; row <= size; row++) {
+                for (int col = 1; col <=row ; col++) {
+                    System.out.print(""+col);
+                }
+                System.out.println(" ");
+            }
+            for (int row = size-1; row >= 1; row--) {
+                for (int col = 1; col <=row ; col++) {
+                    System.out.print(""+col);
+                }
+                System.out.println(" ");
+            }
+        }
+}
