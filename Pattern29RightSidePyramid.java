171890fd116113bc81585473982fabb722112bbe
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-23 19:39:35 +0600

    Pattern Right Side Pyramid

---
+++ Pattern29RightSidePyramid.java
@@ -0,0 +1,24 @@
+package basic;
+import java.util.Scanner;
+
+public class Pattern29RightSidePyramid {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter Line number: ");
+        int line=input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            //for printing space 
+            for (int col = 1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print(row);
+            }
+            System.out.println(" ");
+            
+        }
+        
+        
+    }
+}
