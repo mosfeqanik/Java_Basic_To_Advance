3b1dd3c2a093c34261315aa212d04c705495c5af
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-24 19:21:19 +0600

    Pattern48 Ups and Down Right Side Pyramid Hash

---
+++ Pattern48UpsandDownRightSidePyramidHash.java
@@ -0,0 +1,27 @@
+package basic;
+import java.util.Scanner;
+
+public class Pattern48UpsandDownRightSidePyramidHash {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter Line number:  ");
+        int line=input.nextInt();
+        
+        
+        int result;
+        for (int row = line; row >=1; row--) {
+            for (int space = 1; space <=line-row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <=row; col++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+
+    }
+}
+
+
+
+
