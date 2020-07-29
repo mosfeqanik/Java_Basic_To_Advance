352770306b2c020b3b7fd7d3deac4d142bbbe30d
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-29 22:50:22 +0600

    Pattern30 Left Pointing Arrow ABC

---
+++ Pattern31ArrowExample.java
@@ -0,0 +1,40 @@
+package basic;
+import java.util.Scanner;
+
+public class Pattern31ArrowExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Line number to Draw: ");
+        int line= input.nextInt();
+        int result=0;
+        
+        for (int row = 1; row <=line; row++) {
+            //for printing space 
+            for (int space = 1; space <=line-row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);
+            }
+            System.out.println(" ");
+        }
+        for (int row = line-1; row >=1; row--) {
+            //for printing space 
+            for (int space = 1; space <=line-row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);
+            }
+            System.out.println("");
+        }
+    }    
+}    
+
+
