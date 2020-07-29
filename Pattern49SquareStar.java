540b3c9e9c22735b61a7d7fdb32dae55529f77d6
mosfeqanik <mosfeqanik01@gmail.com>
2020-07-29 23:08:55 +0600

    Pattern Square Star

---
+++ Pattern49SquareStar.java
@@ -0,0 +1,19 @@
+package basic;
+import java.util.Scanner;
+
+public class Pattern49SquareStar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter The lenght of a side of Square : ");
+        int length=input.nextInt();
+        
+        for (int row = 1; row <= length; row++) {
+            for (int col = 1; col <= length; col++) {
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+    }
+
+    
+}
