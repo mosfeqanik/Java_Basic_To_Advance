663e6ef1ad67a4d3701d43e28ff8857870f40dbe
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-09 16:56:44 +0600

    pattern Pyramid upside down with 123 is created

---
+++ PatternsWithLoops/Pattern63PyramidUpandDownwith123.java
@@ -0,0 +1,29 @@
+
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern63PyramidUpandDownwith123 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = line; row >=1; row--) {
+            for (int col =line-row; col >=1; col--) {
+                System.out.print(" ");
+            }
+            for (int col = 2*row-1; col >=1; col--) {
+                System.out.print(row);
+            }
+            System.out.println(" ");
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
+
