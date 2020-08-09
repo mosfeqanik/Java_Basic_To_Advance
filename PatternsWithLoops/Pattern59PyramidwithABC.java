2ec45bd6e600cbfe08a6e329f1ed1e90efc5fbce
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-09 15:44:23 +0600

    pattern Pyramid with ABC is created

---
+++ PatternsWithLoops/Pattern59PyramidwithABC.java
@@ -0,0 +1,34 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner;
+
+public class Pattern59PyramidwithABC {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2*row-1; col++) {
+                int result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);
+            }
+            System.out.println(" ");
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
+
+
+
