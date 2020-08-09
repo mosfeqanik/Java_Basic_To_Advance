6347e9b2a0a50f3cb2ca098901831ea1a5d79d15
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-09 18:35:56 +0600

    pattern Pyramid upside down with ABC is created

---
+++ PatternsWithLoops/Pattern65PyramidUpandDownwithABC.java
@@ -0,0 +1,32 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern65PyramidUpandDownwithABC {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = line; row >=1; row--) {
+            for (int col =line-row; col >=1; col--) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <=2*row-1; col++) {
+                int result=row+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);
+            }
+            System.out.println(" ");
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
+
+
