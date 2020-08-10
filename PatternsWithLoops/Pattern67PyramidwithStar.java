e4f03653e8d98312fc9d70adada66c112ecd1d1f
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-10 23:37:51 +0600

    pattern pyramid with ABC is created

--- PatternsWithLoops/Pattern67PyramidwithStar.java
+++ PatternsWithLoops/Pattern67PyramidwithStar.java
@@ -13,7 +13,7 @@ public class Pattern67PyramidwithStar {
                 System.out.print(" ");
             }
             for (int col = 1; col <= row-1; col++) {
-                System.out.print(row +" ");
+                System.out.print("* ");
             }
             System.out.println(" ");
         }  
