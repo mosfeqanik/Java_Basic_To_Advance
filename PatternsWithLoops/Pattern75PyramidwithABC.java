62598958ac39c6e2f45e4bb4f58f08d9fdc692bd
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-10 23:47:47 +0600

    pattern pyramid with ABC is created

---
+++ PatternsWithLoops/Pattern75PyramidwithABC.java
@@ -0,0 +1,27 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern75PyramidwithABC {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                int result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c+" "); 
+            }
+            System.out.println(" ");
+        }  
+    }
+}
+
+
+
