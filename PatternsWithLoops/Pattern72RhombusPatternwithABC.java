b85ca52d7a2bb469467a4d72fd81a2884f2600c5
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-10 23:31:50 +0600

    pattern Rhombus Pattern with ABC is created

---
+++ PatternsWithLoops/Pattern72RhombusPatternwithABC.java
@@ -0,0 +1,39 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern72RhombusPatternwithABC {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2*row-1; col++) {
+                int result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);               
+            }
+            System.out.println(" ");
+        }
+        for (int row = line-1; row >=1; row--) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2*row-1; col++) {
+                int result=col+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c);  
+            }
+            System.out.println(" ");
+        }   
+    }
+}
+
+
+
