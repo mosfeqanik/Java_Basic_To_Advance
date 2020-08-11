1f19033746f7d6094cc66923e366ada8f97ca7b7
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-11 23:26:36 +0600

    pattern diamond with Star is created

---
+++ PatternsWithLoops/Pattern77DiamondwithStar.java
@@ -0,0 +1,23 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern77DiamondwithStar {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* "); 
+            }
+            System.out.println(" ");
+        }  
+    }
+}
+
+
