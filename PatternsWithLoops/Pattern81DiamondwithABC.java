cd925630448d7f4c4b311661bef45fba39ccf1ba
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-11 23:40:00 +0600

    pattern diamond with ABC is created

---
+++ PatternsWithLoops/Pattern81DiamondwithABC.java
@@ -0,0 +1,40 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner ;
+
+public class Pattern81DiamondwithABC {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Desire Line number: ");
+        int line= input.nextInt();
+        
+        for (int row = 1; row <=line; row++) {
+            for (int col =1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                int result=row+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c+" "); 
+            }
+            System.out.println(" ");
+        } 
+        for (int row = line-1; row >=1; row--) {
+            for (int col = 1; col <=line-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                int result=row+64;
+                //result of integar  then convert to charecter
+                char c=(char)result;
+                System.out.print(c+" ");  
+            }
+            System.out.println(" ");
+        }  
+    }
+}
+
+
+
+
