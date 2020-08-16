555db2ba1e4efc86465afaa4baf80c894514d283
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-16 21:57:22 +0600

    pattern Pyramid Rombos is created

---
+++ PatternsWithLoops/Pattern87Rhombos.java
@@ -0,0 +1,35 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner;
+
+public class Pattern87Rhombos {
+    public static void main(String[] args) {
+        System.out.print("Enter the number of height of the pyramid: ");
+        Scanner input = new Scanner(System.in);
+        int height=input.nextInt();
+        
+        for (int row = 1; row <=height; row++) {
+            for (int col = 1; col <= height-row ; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            for (int col = row-1; col >= 1; col--) {
+                System.out.print(col);
+            }
+            System.out.println(" ");
+        }
+        for (int row = height-1; row >=1; row--) {
+            for (int col = 1; col <= height-row ; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            for (int col = row-1; col >= 1; col--) {
+                System.out.print(col);
+            }
+            System.out.println(" ");
+        }
+    }
+}
