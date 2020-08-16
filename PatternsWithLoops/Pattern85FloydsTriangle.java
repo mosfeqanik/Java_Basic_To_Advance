e63e7c692556462f8605b061ed8ff149c5b03bb4
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-16 20:29:53 +0600

    pattern  Floyd's triangle is created

---
+++ PatternsWithLoops/Pattern85FloydsTriangle.java
@@ -0,0 +1,19 @@
+package basic.PatternsWithLoops;
+import java.util.Scanner;
+
+public class Pattern85FloydsTriangle {
+    public static void main(String[] args) {
+        System.out.print("Enter the number of Triangle Height : ");
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        int count=0;
+        for (int row = 1; row <= height; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(++count +" ");
+            }
+            System.out.println(" ");
+        }
+    
+    
+    }
+}
