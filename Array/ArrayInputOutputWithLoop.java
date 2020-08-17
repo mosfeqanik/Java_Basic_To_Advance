dac3c635c4fe2bf3b99e465dbaeedc750493054a
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-17 21:24:07 +0600

    Array Input/Output with Loop

--- Array/ArrayInputOutputWithLoop.java
+++ Array/ArrayInputOutputWithLoop.java
@@ -1,11 +1,15 @@
 package basic.Array;
+
 import java.util.Scanner;
 
 public class ArrayInputOutputWithLoop {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double[] number= new double[5];
+        
+        double[] number = new double[5];
         double sum = 0;
+        
         System.out.print("Enter Array values : ");
         for (int i = 0; i < number.length; i++) {
             number[i] = input.nextDouble();
@@ -13,12 +17,13 @@ public class ArrayInputOutputWithLoop {
         for (int i = 0; i < number.length; i++) {
             sum = sum + number[i];
         }
+        
         double lenght = number.length;
+        double average = sum / lenght;
         
-        double average= sum/lenght;
-        System.out.println("lenght of Array is :"+lenght);
-        System.out.println("Sum of Array elements is :"+ sum);
+        System.out.println("lenght of Array is :" + lenght);
+        System.out.println("Sum of Array elements is :" + sum);
         System.out.println("Average of Array elements is :" + average);
-        
+
     }
 }
