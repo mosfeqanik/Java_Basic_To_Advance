40edb898fbbfca535cfa56ab3154a3f1632d6ea1
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-17 22:01:07 +0600

    Array Find Max and Min

--- Array/ArrayInputOutputWithLoop.java
+++ Array/ArrayInputOutputWithLoop.java
@@ -6,10 +6,10 @@ public class ArrayInputOutputWithLoop {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         double[] number = new double[5];
         double sum = 0;
-        
+
         System.out.print("Enter Array values : ");
         for (int i = 0; i < number.length; i++) {
             number[i] = input.nextDouble();
@@ -17,10 +17,10 @@ public class ArrayInputOutputWithLoop {
         for (int i = 0; i < number.length; i++) {
             sum = sum + number[i];
         }
-        
+
         double lenght = number.length;
         double average = sum / lenght;
-        
+
         System.out.println("lenght of Array is :" + lenght);
         System.out.println("Sum of Array elements is :" + sum);
         System.out.println("Average of Array elements is :" + average);
