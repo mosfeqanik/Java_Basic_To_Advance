decea100849c1116894512e1c65462b5dd6f80a7
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-19 16:45:32 +0600

    Array Foreach/ enhanced for loop

---
+++ Array/ArrayForEach.java
@@ -0,0 +1,28 @@
+package basic.Array;
+
+public class ArrayForEach {
+
+    public static void main(String[] args) {
+        //Decalaration
+        String[] name;
+        
+        name = new String[3];
+        name[0] = "Md";
+        name[1] = "Mosfeq";
+        name[2] = "Anik";
+
+        int lenght = name.length;
+        System.out.println(lenght);
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.print(name[i] + " ");
+        }
+        System.out.println(" ");
+
+        for (String x : name) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+}
