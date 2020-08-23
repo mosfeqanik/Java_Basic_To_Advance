2810fa8f654851e81d46d0611d22e8e279162784
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-23 13:51:54 +0600

    Array Example with loop

---
+++ Array/ArrayNewExample.java
@@ -0,0 +1,34 @@
+package basic.Array;
+        import java.util.Scanner;
+
+public class ArrayNewExample {
+
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Enter row number: ");
+        int row = input.nextInt() ;
+                int[][] number = new int[row][5];
+                        int k = 0;
+
+
+        
+        //assigning value to array 
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < 5; j++) {
+                number[i][j] = k;
+                k++;
+            }
+        }
+
+        //printing value from the array
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print(number[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
