5256010a86023ae0db60de94f8b61a51f60ac1b6
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-24 21:27:51 +0600

    ArrayList clear

--- Arraylist/ArrayListTopic2.java
+++ Arraylist/ArrayListTopic2.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basic.Arraylist;
 
-/**
- *
- * @author MN
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ArrayListTopic2 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        System.out.println("Size : " + numbers.size());
+        //adding elements to ArrayList
+        numbers.add(12);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(32);
+        numbers.add(333);
+        System.out.println("Size : " + numbers.size());
+        System.out.println(numbers + " ");
+        
+        
+        numbers.removeAll(numbers);
+        System.out.println("after remove all method" + numbers);
+        
+        numbers.add(12);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(32);
+        numbers.add(333);
+        System.out.println("Size : " + numbers.size());
+        System.out.println(numbers + " ");
+        
+        numbers.clear();
+        System.out.println("after clear method" + numbers);
+
+        
+        
+
+    }
 }
