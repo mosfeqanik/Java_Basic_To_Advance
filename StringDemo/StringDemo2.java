41fc3629286ef252a0a84e3889e6aa3a7d04d860
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-25 22:30:26 +0600

    String format-specifiers, several integers to display in the output ,concatenation,startsWith,toUpperCase,toLowerCase,endsWith,String Array

---
+++ StringDemo/StringDemo2.java
@@ -0,0 +1,62 @@
+package basic.StringDemo;
+
+import java.util.Arrays;
+
+public class StringDemo2 {
+
+    public static void main(String[] args) {
+        String firstName = "Mosfeq";
+        String SecondName = " Anik";
+
+        
+        //format-specifiers
+        System.out.printf("Display a Number %d", 15000);
+        
+        //several integers to display in the output
+        System.out.printf("Sum of %d and %d is %d", 15, 40, 55);
+        System.out.printf("My Name is %s. I was born in %d", "Mike", 1998);
+        System.out.println("\n");
+        //concatenation
+        System.out.println("Full name: " + firstName + " " + SecondName);
+        System.out.println(firstName + " 25");
+
+        String Fullname = firstName.concat(SecondName);
+        System.out.println(Fullname);
+
+        String FullName_upperCase = Fullname.toUpperCase();
+        System.out.println(FullName_upperCase);
+
+        String FullName_LowerCase = Fullname.toLowerCase();
+        System.out.println(FullName_LowerCase);
+
+        boolean b = Fullname.startsWith("M");
+        System.out.println("DOES " + Fullname + " starts With M :" + b);
+
+        boolean a = Fullname.startsWith("a");
+        System.out.println("DOES " + Fullname + " starts With a :" + a);
+
+        boolean e = Fullname.endsWith("Anik");
+        System.out.println("DOES " + Fullname + " ends With Anik :" + e);
+        
+        boolean d = Fullname.endsWith("anik");
+        System.out.println("DOES " + Fullname + " ends With anik :" + d);
+        
+        boolean c = Fullname.endsWith("feq");
+        System.out.println("DOES " + Fullname + " ends With feq :" + c);
+        
+        String[] names = {"Anik","jumur","adison","sakib","mosfeq","mahi"};
+        System.out.println(Arrays.toString(names));
+        
+        for (String x:names) {
+            System.out.println(x);
+        }
+        
+        System.out.println("");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        
+    
+    }
+
+}
