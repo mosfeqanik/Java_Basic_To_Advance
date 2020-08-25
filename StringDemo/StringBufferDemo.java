df937dabaa142d983fa5fafbeae1bf388564df1e
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-26 01:02:20 +0600

    StringBuffer,append(),reverse(),delete(),setLength()

---
+++ StringDemo/StringBufferDemo.java
@@ -0,0 +1,31 @@
+package basic.StringDemo;
+
+public class StringBufferDemo {
+
+    public static void main(String[] args) {
+        StringBuffer S1 = new StringBuffer("Mosfeq Anik");
+        System.out.println(S1);
+
+        S1.append(" HI,");
+        S1.append(" How are you?");
+
+        System.out.println(S1);
+
+        S1.reverse();
+        System.out.println(S1);
+
+        StringBuffer S2 = new StringBuffer("MD Mosfeq Anik");
+        
+        System.out.println(S2);
+
+        S2.delete(0, 4);
+        
+        System.out.println(S2);
+        
+        S2 = new StringBuffer("MD Mosfeq Anik");
+        S2.setLength(5);
+        
+        System.out.println(S2);
+        
+    }
+}
--- StringDemo/StringDemo4.java
+++ StringDemo/StringDemo4.java
@@ -14,6 +14,7 @@ public class StringDemo4 {
         String[] s3=s1.split(" ");
         System.out.println(Arrays.toString(s3));
         
+        
         for(String x:s3){
             System.out.println(x);
         }   
