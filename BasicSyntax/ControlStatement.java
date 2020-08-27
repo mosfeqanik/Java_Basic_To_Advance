b828f1d364c50e5c4cf1630b022bdf4daa195106
mosfeqanik <mosfeqanik01@gmail.com>
2020-08-27 14:03:10 +0600

    Conditional Statement

--- BasicSyntax/ControlStatement.java
+++ BasicSyntax/ControlStatement.java
@@ -12,34 +12,7 @@ public class ControlStatement {
         num = input.nextInt();
         System.out.println("number " + num);
 
-        /*
-        if (expression) {
-        // body: do something
-        }   */
-        int age = 120; // it has a value
-        if (age > 100) {
-            System.out.println("Very experienced person");
-        }
-
-        boolean b = true; // it is true or false
-        if (b) { // or !b
-            // do something
-            System.out.println("it is true");
-
-        }
-
-        long dollars = 200_000; // your budget
-
-        if (dollars < 1000) {
-            System.out.println("Buy a laptop");
-        } else if (dollars < 2000) {
-            System.out.println("Buy a personal computer");
-        } else if (dollars < 100_000) {
-            System.out.println("Buy a server");
-        } else {
-            System.out.println("Buy a data center or a quantum computer");
-        }
-
+        
         //even or number
         if (num > 0) {
             System.out.println(num + ",The number is positive");
