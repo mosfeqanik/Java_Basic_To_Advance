115f82176bc73e9a7ff86f69c45ccf5ad5e2eb83
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-01 22:35:09 +0600

    String to Primitive datatype

--- intermediate/Convertion_String_To_Any_Other_dataType.java
+++ intermediate/Convertion_String_To_Any_Other_dataType.java
@@ -7,6 +7,14 @@ public class Convertion_String_To_Any_Other_dataType {
         int j = Integer.parseInt(i);
         System.out.println("j = "+j );
     
+        int e = Integer.valueOf(i);
+        System.out.println("e = "+e );
         
+        String d = "30.55";
+        double r = Double.parseDouble(d);
+        System.out.println("r = "+r );
+        
+        double t = Double.valueOf(d);
+        System.out.println("t = "+t );
     }
 }
