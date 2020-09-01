122cb99cd6647b39d9fdf84162a12d9936331dc5
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-01 22:29:19 +0600

    primitive data type to String

--- intermediate/ConvertionToString.java
+++ intermediate/ConvertionToString.java
@@ -5,15 +5,15 @@ public class ConvertionToString {
     public static void main(String[] args) {
         int i = 100;
         String s = Integer.toString(i);
-        System.out.println("s ="+s);
+        System.out.println("s = "+s);
         
         double d = 100.33;
         String f = Double.toString(d);
-        System.out.println("f ="+f);
+        System.out.println("f = "+f);
         
         boolean b = true;
         String g = Boolean.toString(b);
-        System.out.println("g ="+g);
+        System.out.println("g = "+g);
         
     }
 }
