371b5363ecbd53ae0bfe95e424f68f8791474605
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-01 23:19:24 +0600

    Date & formatter

--- intermediate/DateExample.java
+++ intermediate/DateExample.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic.intermediate;
 
-/**
- *
- * @author MN
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateExample {
-    
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/YYYY");
+
+        String SimpleDate = dateformat.format(date);
+        System.out.println("Current Date"+SimpleDate);
+    }
 }
