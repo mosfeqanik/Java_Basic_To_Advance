a2f4e5a25f804eb4f4c11a509e2b9a7d04e3928f
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-02 18:20:30 +0600

    do while demo

--- BasicSyntax/DoWhileDemo.java
+++ BasicSyntax/DoWhileDemo.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic.BasicSyntax;
 
 import java.util.Scanner;
 
-/**
- *
- * @author MN
- */
 public class DoWhileDemo {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -21,4 +13,5 @@ public class DoWhileDemo {
             System.out.println(value);
         } while (value != 0);
     }
+
 }
