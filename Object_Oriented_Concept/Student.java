6d89b8da0d03c2ad2666ef3c9f59063813f2b06b
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-11 19:44:10 +0600

    Method using

--- Object_Oriented_Concept/Student.java
+++ Object_Oriented_Concept/Student.java
@@ -3,4 +3,13 @@ public class Student {
     String name,Class_name,Address,roll;
     int age,phone;
     
+    void displayInfortamtion(){
+        System.out.println("Anik's roll is " + roll);
+        System.out.println("Anik's Class_name is " + Class_name);
+        System.out.println("Anik's name is " + name);
+        System.out.println("Anik's phone is " + phone);
+        System.out.println("Anik's Address is " + Address);
+        System.out.println("Anik's age is " + age);
+    }
+    
 }
