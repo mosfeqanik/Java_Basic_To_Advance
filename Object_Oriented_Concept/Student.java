be200af2d29b74e1d20328f269ab084047dddcb3
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-11 19:53:22 +0600

    Method Implement and using

--- Object_Oriented_Concept/Student.java
+++ Object_Oriented_Concept/Student.java
@@ -4,12 +4,12 @@ public class Student {
     int age,phone;
     
     void displayInfortamtion(){
-        System.out.println("Anik's roll is " + roll);
-        System.out.println("Anik's Class_name is " + Class_name);
-        System.out.println("Anik's name is " + name);
-        System.out.println("Anik's phone is " + phone);
-        System.out.println("Anik's Address is " + Address);
-        System.out.println("Anik's age is " + age);
+        System.out.println("Student's roll is " + roll);
+        System.out.println("Student's Class_name is " + Class_name);
+        System.out.println("Student's name is " + name);
+        System.out.println("Student's phone is " + phone);
+        System.out.println("Student's Address is " + Address);
+        System.out.println("Student's age is " + age);
         
         System.out.println("\n  ");
     }
