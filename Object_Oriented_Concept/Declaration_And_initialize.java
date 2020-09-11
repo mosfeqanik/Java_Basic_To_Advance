01a13b75a7ed3eb72cd3b5035aa372540ad1a8ef
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-11 19:41:45 +0600

    Declaration & initialize

--- Object_Oriented_Concept/Declaration_And_initialize.java
+++ Object_Oriented_Concept/Declaration_And_initialize.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic.Object_Oriented_Concept;
 
-/**
- *
- * @author MN
- */
 public class Declaration_And_initialize {
-    
+
+    public static void main(String[] args) {
+        Student Anik = new Student();//Declaration with initialize
+
+        Anik.roll = "1232";
+        Anik.Class_name = "upper Class";
+        Anik.name = "Anik";
+        Anik.phone = 1778881448;
+        Anik.Address = "Mirpur";
+        Anik.age = 25;
+
+        System.out.println("Anik's roll is " + Anik.roll);
+        System.out.println("Anik's Class_name is " + Anik.Class_name);
+        System.out.println("Anik's name is " + Anik.name);
+        System.out.println("Anik's phone is " + Anik.phone);
+        System.out.println("Anik's Address is " + Anik.Address);
+        System.out.println("Anik's age is " + Anik.age);
+
+    }
 }
