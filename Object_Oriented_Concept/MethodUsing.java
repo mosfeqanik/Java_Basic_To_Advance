2215190175d5b9c98b4b7888a8a3214ddcad44e4
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-11 19:46:52 +0600

    Method Implement and using

--- Object_Oriented_Concept/MethodUsing.java
+++ Object_Oriented_Concept/MethodUsing.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic.Object_Oriented_Concept;
 
-/**
- *
- * @author MN
- */
 public class MethodUsing {
-    
+    public static void main(String[] args) {
+        Student Anik = new Student();//Declaration with initialize
+
+        Anik.roll = "1232";
+        Anik.Class_name = "upper Class";
+        Anik.name = "Anik";
+        Anik.phone = 1778881448;
+        Anik.Address = "Mirpur";
+        Anik.age = 25;
+        
+        Anik.displayInfortamtion();
+    }
 }
