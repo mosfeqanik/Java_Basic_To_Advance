9826df3e2777806ba3bd5a7aa033d1af524f2bb2
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-12 21:21:13 +0600

    Constructor started

--- Object_Oriented_Concept/Declaration_And_initialize_With__constructor.java
+++ Object_Oriented_Concept/Declaration_And_initialize_With__constructor.java
@@ -6,5 +6,7 @@ public class Declaration_And_initialize_With__constructor {
         Student1 Anik = new Student1("Anik","upper Class","Mirpur","rl",25,1778881448);//Declaration with initialize
         Anik.displayInfortamtion();
 
+        Student1 anan = new Student1("Anan","Supreme Class","Mirpur","r2",10,1818231208);//Declaration with initialize
+        anan.displayInfortamtion();
     }
 }
