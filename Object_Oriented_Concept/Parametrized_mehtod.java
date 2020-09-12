6fc1a2265417e923b1003aeff21d282d88d57dd2
mosfeqanik <mosfeqanik01@gmail.com>
2020-09-12 19:34:49 +0600

    parametrized Method Implement and using

---
+++ Object_Oriented_Concept/Parametrized_mehtod.java
@@ -0,0 +1,10 @@
+package basic.Object_Oriented_Concept;
+
+public class Parametrized_mehtod {
+    public static void main(String[] args) {
+        Student anik = new Student();
+        anik.setInformation("ANik","Upperclass","Mirpur","12",25,1778881448);
+        anik.displayInfortamtion();
+        
+    }
+}
--- Object_Oriented_Concept/Student.java
+++ Object_Oriented_Concept/Student.java
@@ -1,17 +1,29 @@
 package basic.Object_Oriented_Concept;
+
 public class Student {
-    String name,Class_name,Address,roll;
-    int age,phone;
-    
-    void displayInfortamtion(){
+
+    String name, Class_name, Address, roll;
+    int age, phone;
+
+    void setInformation(String n, String C_name, String address, String rl, int ag, int pn) {
+        name = n;
+        Class_name = C_name;
+        Address = address;
+        roll = rl;
+        age = ag;
+        phone = pn;
+
+    }
+
+    void displayInfortamtion() {
         System.out.println("Student's roll is " + roll);
         System.out.println("Student's Class_name is " + Class_name);
         System.out.println("Student's name is " + name);
         System.out.println("Student's phone is " + phone);
         System.out.println("Student's Address is " + Address);
         System.out.println("Student's age is " + age);
-        
+
         System.out.println("\n  ");
     }
-    
+
 }
